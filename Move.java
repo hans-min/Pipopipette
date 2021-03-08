7edd4743d7dada3130fc2a9e4075d61f98e0bc0d
Hanji <kaitothedraggy@gmail.com>
2021-03-08 21:47:28 +0100

    Update Move.java

--- Move.java
+++ Move.java
@@ -4,29 +4,32 @@ public class Move {
 	int ligne;
 	int colonne;
 	Tableau1 Tableau;
-	int p = 1;                    // joueur 1 ou 2
-	int [] joueur = new int [2]; // tableau contenant le point des joueurs
+	int p = 1;                                                             // joueur 1 ou 2
+	int [] joueur = new int [2];                                           // tableau contenant le point des joueurs
 	
 	public Move() {
 		this.Tableau = new Tableau1();
-		while(Tableau.move >0){ // le nombre des mouvements possibles restant > 0
+		while(Tableau.move >0){                                        // quand le nombre des mouvements possibles restant > 0
     Scanner sc = new Scanner(System.in);
+			
     System.out.println("C'est votre tour,joueur "+p);
     System.out.println("Choisissez la ligne de votre mouvement:");
-    while (!Checkmove()) { // pour vérifier si le mouvement est validé
-    this.ligne = sc.nextInt();
+			
+    while (!Checkmove()) {                                                           // si le mouvement est validé
+    this.ligne = sc.nextInt();                                                       // choose the row
+	    
     while (ligne<0 || ligne>=Tableau.Tableau.length) {
 		  System.out.print("Ei bien sur vous devez choisir un numero valide: ");
-		  this.ligne = sc.nextInt();
+		  this.ligne = sc.nextInt();                                                         //choose again
     }
     System.out.println("Choisissez la colonne de votre mouvement"); 
-    this.colonne =sc.nextInt();
-    while (colonne<0 || colonne>=Tableau.Tableau[0].length) {
+    this.colonne =sc.nextInt();                                                            //choose the column
+    while (colonne<0 || colonne>=Tableau.Tableau[0].length) {                                                        
 		  System.out.print("Ei bien sur vous devez choisir un numero valide: ");
 		  this.colonne = sc.nextInt();
     }
 }
-    write(); // remplir la position choisie avec | ou -
+    write(); // remplir la position choisie avec | ou - (pour votre movement)
     Tableau.affiche(); // afficher le tableau
     Entoure(); // pour vérifier si le joueur a marqué un point òu le tour passe à l'autre joueur
     
@@ -34,11 +37,12 @@ public class Move {
     }
     declare(joueur); //declaré le gagnant
 }
-    public boolean Checkmove() { // vérifier si le mouvement choisie est validé
-	    if ((this.ligne + this.colonne)%2==0) { // == position du mouvement est celle d'un * ou un carré
+	
+    public boolean Checkmove() {                                                        // vérifier si le mouvement choisie est validé
+	    if ((this.ligne + this.colonne)%2==0) {                                    // == position du mouvement est celle d'un * ou un carré
             System.out.println ("Uh uh choisissez une autre position: ");
             return false;
-	    } else if(this.Tableau.Tableau[this.ligne][this.colonne] != " ") { // position déjà choisie
+	    } else if(this.Tableau.Tableau[this.ligne][this.colonne] != " ") {         // position déjà choisie
             System.out.println( "Choisissez une autre move");
             return false;
         } else {
@@ -46,7 +50,7 @@ public class Move {
             return true;
         }
     }
-  public void write () { // décider s'il faut un - ou un | et le remplir
+  public void write () {                                                            // décider s'il faut un - ou un | et le remplir
     	 if (this.ligne % 2 == 0) { 
     		 this.Tableau.Tableau[this.ligne][this.colonne] = "-";
     	 } else {
@@ -55,30 +59,30 @@ public class Move {
     }
           
             
-    public void Entoure () { // après un mouvement, il faut vérifier 2 cases (carrés)
-         int k = joueur [p-1]+2; // pour garder la note avant le mouvement du joueur
-         if (this.Tableau.Tableau[this.ligne][this.colonne] == "-") { // si c'est un -
-            for (int i = this.ligne-1; i<= this.ligne+1; i++) { // il faudra vérifier les cases au-dessus et en dessous
-                if ((i+this.colonne)%2==0 && i > 0 && i < Tableau.Tableau.length) { // ne vérifie pas la cases contenant le - et celles qui dépassent le tableau 
-                   EstEntoure(i,this.colonne); // pour vérifier si un carré est rempli (avec les 3 autres côtés)
+    public void Entoure () {                                                           // après un mouvement, il faut vérifier 2 cases (carrés)
+         int k = joueur [p-1]+2;                                                       // pour garder la note avant le mouvement du joueur
+         if (this.Tableau.Tableau[this.ligne][this.colonne] == "-") {                  // si c'est un '-'
+            for (int i = this.ligne-1; i<= this.ligne+1; i++) {                        // il faudra vérifier les cases au-dessus et en dessous
+                if ((i+this.colonne)%2==0 && i > 0 && i < Tableau.Tableau.length) {    // ne vérifie pas la cases contenant le - et celles qui dépassent le tableau 
+                   EstEntoure(i,this.colonne);                                         // pour vérifier si un carré est rempli (avec les 3 autres côtés)
                 }
             }
         } else if (this.Tableau.Tableau[this.ligne][this.colonne] == "|") { 
-            for ( int j=this.colonne-1; j <= this.colonne+1;j++) { // les cases à gauche et à droite 
+            for ( int j=this.colonne-1; j <= this.colonne+1;j++) {                     // si c'est un '|', vérifier les cases à gauche et à droite 
                 if ((j+this.ligne)%2==0 && j >0 && j < Tableau.Tableau[0].length) {
                     EstEntoure(this.ligne,j);
                 }
             }
         }
-        if (k-joueur[p-1] == 2){ // si le joueur ne gagne aucun point
-				p = (p % 2) + 1; //changer joueur
+        if (k-joueur[p-1] == 2){                                                           // si le joueur ne gagne aucun point
+				p = (p % 2) + 1;                                           //changer joueur
         }
     }
     
     
-    public void EstEntoure (int lig, int col) { // vérifier si un carré est complète pour ajouter des points
+    public void EstEntoure (int lig, int col) {                                    // vérifier si un carré est complète pour ajouter des points
         boolean EstEntoure= true;
-        for (int i =lig -1;i<= lig+1; i++)  { // balayer l'entourage du carré
+        for (int i =lig -1;i<= lig+1; i++)  {                                      // balayer l'entourage du carré
             for (int j=col-1;j<= col+1;j++) {
                 if (((i+j)%2) != 0 && Tableau.Tableau[i][j] == " ") {
                     EstEntoure= false;
@@ -86,13 +90,15 @@ public class Move {
             }
         }
         if (EstEntoure == true) { 
-            joueur[p-1] = joueur [p-1] + 1;// ajouter point
-            System.out.println(" Point de joueur 1 : +"+joueur [0]);
+            joueur[p-1] = joueur [p-1] + 1;                                          // ajouter point
+            System.out.println(" Point de joueur 1 : +"+joueur [0]);                 //chaque fois quelqu'un gagne un point, print les points des 2 joueurs
             System.out.println(" Point de joueur 2 : +"+ joueur[1]);
         }
     }
     
-    public void declare (int [] joueur) { //déclaré le gagnant
+	
+	
+    public void declare (int [] joueur) {                                            //déclaré le gagnant
     	 if (joueur[0] > joueur [1]) {
     		 System.out.println("Joueur 1 a gagne!!");
     	 } else if (joueur[1] > joueur [0]) {
